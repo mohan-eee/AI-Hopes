a3d4eef8d1fa624d9e5a8bfcd1fb90e4f2cc7951
mohan-eee <mohan.eee007@gmail.comm>
2023-10-01 01:06:38 +0530

    update changes to Level2

---
+++ python/assignment_1_control_structures/Extra-Assignment-2-Level-2-python.py
@@ -0,0 +1,11 @@
+# print 0 to 20 by using range
+for i in range(0,20):
+    print(i)
+
+# print range 10 to 20 
+for i in range(10,20):
+    print(i,end="\n")
+
+# Print number of items in the list by using 'len' 
+list=[10,20,14,55,43,87,76]
+print("No of Elements in list", len(list))
