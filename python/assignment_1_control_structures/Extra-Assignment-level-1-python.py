5001698f8e1ae9e5e427ae444df174412d815671
mohan-eee <mohan.eee007@gmail.comm>
2023-09-30 16:28:38 +0530

    extra assign

---
+++ python/assignment_1_control_structures/Extra-Assignment-level-1-python.py
@@ -0,0 +1,57 @@
+#print Statement
+print("HOPE AI")
+
+# Buy input
+name=input("Enter your institute name? :")
+print("Enter your institute name? :",name)
+
+#Buy Inputs
+name=input('Enter your name : ')
+age=int(input('Enter your age : '))
+sch=input("Enter your School Name : ")
+deg=input("Enter your degree : ")
+
+#Addition
+a=43
+b=34
+add=a+b
+print("add =",add)
+
+#Subtraction
+a=43
+b=34
+sub=a-b
+print("sub =",sub)
+
+#Multiplication
+a=76
+b=31
+mul=a*b
+print("mul =",mul)
+
+#Division
+a=76
+b=31
+div=a/b
+print("div =",div)
+
+#Floor division
+a=76
+b=31
+div=a//b
+print("Div =",div)
+
+#Modulo
+a=76
+b=31
+modulo=a%b
+print("Modulo =",modulo)
+
+#power
+a=4
+b=4
+power=a**b
+print("Power =",power)
+
+
+
